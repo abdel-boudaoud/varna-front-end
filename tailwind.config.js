8a67103aa4109906d018d2dc23ab3348d863e481
abdel-boudaoud <abdelkaderfarm@gmail.com>
2023-08-23 23:36:25 +0100

    add simple version of the hero section and header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor:{
+        "primary":"#004E7C"
+      }
+    },
   },
   plugins: [],
 }
